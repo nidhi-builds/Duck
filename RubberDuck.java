17fe923443dd8170d44aff514c8999e086c87a05
nidhi-builds <nidhipandit2907@gmail.com>
2025-03-08 22:41:34 +0530

    Add RubberDuck.java

---
+++ RubberDuck.java
@@ -0,0 +1,12 @@
+public class RubberDuck extends Duck{
+    public RubberDuck()
+    {
+        quackBehaviour=new Squeak();
+        swimBehaviour=new Floating();
+        flyBehaviour = new NoFlying();
+    }
+    @Override
+    public void display() {
+        System.out.println("I am Rubber Duck...");
+    }
+}
