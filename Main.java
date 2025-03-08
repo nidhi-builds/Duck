79cfdac34164ed1f4b9e77d9076f1f77fd3a28b9
nidhi-builds <nidhipandit2907@gmail.com>
2025-03-08 22:35:58 +0530

    Add Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+/*
+Nidhi Pandit
+23070126081
+AIML B1
+ */
+public class Main {
+    public static void main(String[] args) {
+        RubberDuck rb = new RubberDuck();
+        System.out.println("------------");
+        rb.display();
+        rb.performQuack();
+        rb.performSwim();
+        rb.performFly();
+        System.out.println("------------");
+        MallardDuck md = new MallardDuck();
+        System.out.println("------------");
+        md.display();
+        md.performQuack();
+        md.performSwim();
+        md.performFly();
+        System.out.println("------------");
+
+
+        }
+    }
